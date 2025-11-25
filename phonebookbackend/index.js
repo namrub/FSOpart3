9e82f920bbcbebe9d11feb25aa72b9c708a6843e
Namrub <frankburman@edu.novia.fi>
2025-11-25 19:30:34 +0200

    added dist

--- phonebookbackend/index.js
+++ phonebookbackend/index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.static('dist'))
 
 morgan.token('body', request => {
 	return JSON.stringify(request.body)
