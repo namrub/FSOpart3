cdd419fe9f7665a3fa31c723535d31c4d5f63138
Namrub <frankburman@edu.novia.fi>
2025-11-27 09:11:14 +0200

    minor error handling changes to notes app

--- notesbackend/index.js
+++ notesbackend/index.js
@@ -36,7 +36,7 @@ app.get('/api/notes/:id', (request, response, next) => {
 })
 
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
 	const body = request.body
 
 	if (!body.content) {
@@ -51,6 +51,7 @@ app.post('/api/notes', (request, response) => {
 	note.save().then(savedNote => {
 		response.json(savedNote)
 	})
+		.catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
@@ -92,6 +93,8 @@ const errorHandler = (error, request, response, next) => {
 
 	if (error.name === 'CastError') {
 		return response.status(400).send({ error: 'malformatted id' })
+	} else if (error.name === 'ValidationError') {
+		return response.status(400).json({ error: error.message })
 	}
 
 	next(error)
