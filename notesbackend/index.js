6988635a37f311997b16c1a59d4ec0b9d617fd9d
Namrub <frankburman@edu.novia.fi>
2025-11-27 07:50:24 +0200

    fixed delete, added put option, made error handler

--- notesbackend/index.js
+++ notesbackend/index.js
@@ -4,8 +4,8 @@ const app = express()
 const morgan = require('morgan')
 const Note = require('./models/note.js')
 
-app.use(express.json())
 app.use(express.static('dist'))
+app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 morgan.token('body', request => {
@@ -24,10 +24,15 @@ app.get('/api/notes', (request, response) => {
 	})
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
 	Note.findById(request.params.id).then((note) => {
-		response.json(note)
+		if (note) {
+			response.json(note)
+		} else {
+			response.status(404).end()
+		}
 	})
+		.catch(error => next(error))
 })
 
 
@@ -48,17 +53,54 @@ app.post('/api/notes', (request, response) => {
 	})
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-	const id = request.params.id
-	notes = notes.filter((note) => note.id !== id)
+app.put('/api/notes/:id', (request, response, next) => {
+	const { content, important } = request.body
 
-	response.status(204).end()
+	Note.findById(request.params.id)
+		.then(note => {
+			if (!note) {
+				return response.status(404).end()
+			}
+
+			note.content = content
+			note.important = important
+
+			return note.save().then((updatedNote) => {
+				response.json(updatedNote)
+			})
+		})
+		.catch(error => next(error))
 })
+
+app.delete('/api/notes/:id', (request, response, next) => {
+	Note.findByIdAndDelete(request.params.id)
+		.then(result => {
+			response.status(204).end()
+		})
+		.catch(error => next(error))
+})
+
 const unknownEndpoint = (request, response) => {
 	response.status(404).send({ error: 'unknown endpoint' })
 }
 
+// handler of requests with unknown endpoint
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+	console.error(error.message)
+
+	if (error.name === 'CastError') {
+		return response.status(400).send({ error: 'malformatted id' })
+	}
+
+	next(error)
+}
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+// handler of requests with result to errors
+app.use(errorHandler)
+
 const PORT = process.env.PORT
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
